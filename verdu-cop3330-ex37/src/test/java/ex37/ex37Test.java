aac8a079c211b390db7f3f9481957b00d0cc3bfa
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-09-26 18:01:57 -0400

    Add files via upload

---
+++ verdu-cop3330-ex37/src/test/java/ex37/ex37Test.java
@@ -0,0 +1,74 @@
+package ex37;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ex37Test {
+
+    public static void main(String[] Args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> Password = new ArrayList<String>();
+
+        System.out.print("What's the minimum length? ");
+        String min_length = scanner.nextLine();
+
+        System.out.print("How many special characters? ");
+        String special = scanner.nextLine();
+
+        System.out.print("How many numbers? ");
+        String numbers = scanner.nextLine();
+
+        int Length = Integer.parseInt(min_length);
+        int Special = Integer.parseInt(special);
+        int Numbers = Integer.parseInt(numbers);
+        int Letters = Length - (Special + Numbers);
+
+        if (Letters < (Special + Numbers)) {
+            System.out.print("The number of letters must be equal to or bigger than the number of special characters and numbers.");
+            System.exit(0);
+        }
+
+        Array_Generator(Password, Letters, Numbers, Length);
+        Collections.shuffle(Password);
+
+        System.out.print("Your password is ");
+
+        for(int i =0 ; i<Length; i++){
+            System.out.print(Password.get(i));
+        }
+
+    }
+
+    public static void Array_Generator(ArrayList<String> Password, int Letters, int Numbers, int Length){
+        Random rand = new Random();
+
+        for (int i = 0; i < Letters; i++) {
+            char random_letter = (char) ('a' + rand.nextInt(26));
+            String s1 = String.valueOf(random_letter);
+            Password.add(s1);
+        }
+
+        for (int j = Letters; j < Letters + Numbers; j++) {
+            int random_number = rand.nextInt(10);
+            String s2 = String.valueOf(random_number);
+            Password.add(s2);
+
+        }
+
+        for (int k = Letters + Numbers; k < Length; k++) {
+            char random_special = (char) ('!' + rand.nextInt(15));
+            String s3 = String.valueOf(random_special);
+            Password.add(s3);
+        }
+
+
+    }
+
+
+
+}
